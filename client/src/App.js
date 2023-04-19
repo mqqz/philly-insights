7b3069c9bbecc964b5184a8e1fcd30c8616b29ff
m0hdas <m.alsadhan@sms.ed.ac.uk>
2023-04-19 13:42:43 -0400

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
+import styles from "./index.css" // needed for tailwindcss
 import Heatmap from "./components/Heatmap";
-import styles from "./index.css"
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Philly Insights</h1>
-        A data-driven look at the City of Brotherly Love
+      A data-driven look at the City of Brotherly Love
       </header>
       <Heatmap />
     </div>
