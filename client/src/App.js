0ca86f6e4817f6acdb7853dca4972e4e621c0d2e
m0hdas <m.alsadhan@sms.ed.ac.uk>
2023-04-19 11:05:21 -0400

    install tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Heatmap from "./components/Heatmap";
+import styles from "./index.css"
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Philly Insights</h1>
+        <h1 className="text-3xl font-bold underline">Philly Insights</h1>
         A data-driven look at the City of Brotherly Love
       </header>
       <Heatmap />
