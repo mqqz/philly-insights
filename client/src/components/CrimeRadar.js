d87ddb21b7614b07932bf62230b338e2beab0431
m0hdas <m.alsadhan@sms.ed.ac.uk>
2023-05-01 06:01:46 -0400

    added two charts tohomepage

--- client/src/components/CrimeRadar.js
+++ client/src/components/CrimeRadar.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect, PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
-export default function CrimeRadar(){
+export default function CrimeRadar({violence}){
     
         const [data, setPoints] = useState([]);
 
         const endpoint = process.env.API_ENDPOINT || 'http://localhost:8000';
         useEffect(() => {
-            fetch(endpoint + '/api/crimeByType')
+            fetch(endpoint + '/api/crimeByType?isViolent=' + violence)
                 .then(res => res.json())
-                .then(data => {
+              .then(data => {
                     setPoints(data);
                 });
         }, []);
-
-    return (
-      <ResponsiveContainer width="100%" height="100%">
+  return (
+      <ResponsiveContainer height={250}>
         <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="crime_type" />
           <PolarRadiusAxis />
-          <Radar name="crimes" dataKey="crime_count" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+          <Radar name="crime_count" dataKey="crime_count" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
         </RadarChart>
       </ResponsiveContainer>
     );
--- server/src/api/crimeByType.js
+++ server/src/api/crimeByType.js
@@ -5,14 +5,20 @@ const router = express.Router();
 // get the database connection pool
 const pool = require('../db');
 //THIS IS QUERY 5
+
 router.get('/', (req, res) => {
+  // modified query slightly to include violent crimes
+  let isViolent = req.query.isViolent == "true" ?? false;
+  let violentCrimes = ["\'Robbery Firearm\'", "\'Robbery No Firearm\'",
+    "\'Homicide - Criminal\'",
+    "\'Aggravated Assault No Firearm\'", "\'Aggravated Assault Firearm\'"];
+  let nonViolentCrimes = ["\'Thefts\'", "\'Embezzlement\'", "\'Fraud\'",
+    "\'Vandalism\'", "\'DRIVING UNDER THE INFLUENCE\'"];
+  
   const query = `SELECT text_general_code AS crime_type, COUNT(*) AS crime_count
   FROM Crime
-  WHERE text_general_code = 'Thefts' OR text_general_code = 'Embezzlement' OR        
-         text_general_code = 'Fraud' OR text_general_code = 'Vandalism' OR text_general_code = 'DRIVING UNDER THE INFLUENCE'
+  WHERE text_general_code IN (${isViolent ? violentCrimes : nonViolentCrimes})
   GROUP BY text_general_code
-  ORDER BY COUNT(*) DESC
-
   `;
 
   pool.getConnection((err, connection) => {
